39df5c05f6f5e17adf431ca6b2d79f36a8034016
huangkaren <karenhuang@uchicago.edu>
2024-10-27 23:04:43 +0000

    finish

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -14,16 +14,15 @@ class ZebraController < ApplicationController
     render({ :template => "game_templates/square_results"})
   end
 
-  # Square Root
+# Square Root
   def square_root 
     render({ :template => "game_templates/square_root"})
   end
 
   def square_root_results 
-    @the_num = params.fetch("users_number").to_f
-    @the_result = (@the_num ** 0.5)
-    
-    render({ :template => "game_templates/square_root_results"})
+  @the_num = params.fetch("users_number").to_f
+  @the_result = (@the_num ** 0.5).to_f
+  render({ :template => "game_templates/square_root_results"})
   end
 
 #Payment
@@ -32,6 +31,10 @@ class ZebraController < ApplicationController
   end
 
   def payment_results 
+    @apr = params.fetch("user_apr").to_f
+    @years = params.fetch("user_years").to_i 
+    @principal = params.fetch("user_principal").to_f
+    @payment = ((@apr / (100 * 12))*@principal) / (1 - ((1 + (@apr / (100 * 12))) ** (-@years * 12)))
     render({ :template => "game_templates/payment_results"})
   end
 
@@ -41,6 +44,9 @@ class ZebraController < ApplicationController
   end
 
   def random_results 
+    @minimum = params.fetch("user_min").to_f
+    @maximum = params.fetch("user_max").to_f 
+    @random = rand(@minimum..@maximum)
     render({ :template => "game_templates/random_results"})
   end
 
